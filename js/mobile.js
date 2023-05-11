ea7703fb2ee081fcd97ac94bbc2a3d6e4193a2fc
ProjingPead <132311093+ProjingPead@users.noreply.github.com>
2023-05-11 14:21:22 -0300

    Add files via upload

--- js/mobile.js
+++ js/mobile.js
@@ -1,5 +1,9 @@
 const banner = document.querySelector(".banners")
 let slide = 0;
+const banner1 = "url('../img/Mobile/banner1.png')"
+const banner2 = "url('../img/Mobile/banner2.png')"
+const banner3 = "url('../img/Mobile/banner3.png')"
+const banner4 = "url('../img/Mobile/banner4.png')"
 
 function ChangeSlides() {
     slide++;
@@ -7,13 +11,13 @@ function ChangeSlides() {
     console.log(slide);
 
     if (slide === 1) {
-        banner.style.backgroundColor = "red"
+        banner.style.backgroundImage = banner1
     } else if (slide === 2) {
-        banner.style.backgroundColor = "blue"
+        banner.style.backgroundImage = banner2
     } else if (slide === 3) {
-        banner.style.backgroundColor = "Green"
+        banner.style.backgroundImage = banner3
     } else if (slide === 4) {
-        banner.style.backgroundColor = "white"
+        banner.style.backgroundImage = banner4
     }
 }
 
--- js/services.js
+++ js/services.js
@@ -1,8 +1,13 @@
 const voltar = document.querySelector("#voltar")
-const pageService = document.querySelector(".services_description")
+const pageService = document.querySelector(".services_description_page")
 
 voltar.addEventListener("click", e => {
-    pageService.style.display = "none"
+    pageService.setAttribute('class', 'services_description_page animate__animated animate__slideOutDown')
+    setTimeout(function () {
+        pageService.style.display = "none"
+        pageService.setAttribute('class', 'services_description_page animate__animated animate__slideInUp')
+    }, 900);
+
 })
 
 function capturaCliques() {
